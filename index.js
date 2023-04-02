1bca5a258f8c25b8297082f5c7a446e4e05dd764
Deividas <krdeividas@gmail.com>
2023-04-02 17:26:48 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";  
+import cors from "cors";
+
+const configuration = new Configuration({
+    organization: "org-vvdVslwZcM8zbWFmWPGJSPWP",
+    apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/", async (req, res) => {
+    const { messages } = req.body;
+  
+    console.log(messages);
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "You are named Lola and were created in a lab and won't mention openai " },
+        ...messages,
+      ],
+    });
+  
+    res.json({
+      completion: completion.data.choices[0].message,
+    });
+  });
+  
+
+ app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+ });
+
